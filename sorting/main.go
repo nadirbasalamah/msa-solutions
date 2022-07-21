b58257016bfde9ae2bc66cc38e78dc267b87dbcb
nadirbasalamah <nadir008basalamah@gmail.com>
2022-07-21 15:48:04 +0700

    feat(sorting): Adding solution for sorting problem. Menambahkan solusi untuk soal mengenai sorting.

---
+++ sorting/main.go
@@ -0,0 +1,59 @@
+package main
+
+import "fmt"
+
+func main() {
+	var sample []int = []int{1, 4, 5, 6, 8, 2}
+
+	fmt.Println("before sort: ", sample)
+	fmt.Println("ascending sort: ", sort(sample))
+	fmt.Println("descending sort: ", reverseSort(sample))
+}
+
+func sort(data []int) []int {
+	var n int = len(data)
+
+	for i := 1; i < n; i++ {
+		var key int = data[i]
+		var j int = i - 1
+		for j >= 0 && data[j] > key {
+			data[j+1] = data[j]
+			j = j - 1
+		}
+		data[j+1] = key
+	}
+
+	return data
+}
+
+func reverseSort(data []int) []int {
+	var n int = len(data)
+
+	for i := 1; i < n; i++ {
+		var key int = data[i]
+		var j int = i - 1
+		for j >= 0 && data[j] < key {
+			data[j+1] = data[j]
+			j = j - 1
+		}
+		data[j+1] = key
+	}
+
+	return data
+}
+
+func visualize(data []int) {
+	for _, val := range data {
+		fmt.Println(val, createBar(val))
+	}
+}
+
+func createBar(n int) string {
+	var bar string = ""
+
+	for i := 1; i <= n; i++ {
+		bar += "-"
+	}
+
+	return bar
+}
